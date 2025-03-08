2c253fddea5713c9f1aee3ead5676e13929bb451
yoyowongwmy <yoyowong.wmy@gmail.com>
2025-03-08 14:09:58 +1100

    black ruff fixed

--- django_backend/chatbot/views.py
+++ django_backend/chatbot/views.py
@@ -5,50 +5,43 @@ import os
 from openai import OpenAI
 
 load_dotenv()
-openai_api_key = os.getenv('OPENAI_API_KEY')
+openai_api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=openai_api_key)
 
 
 def ask_openai(message):
     response = client.chat.completions.create(
-        model = 'gpt-3.5-turbo',
-        messages = message,
-        max_tokens = 100,
+        model="gpt-3.5-turbo",
+        messages=message,
+        max_tokens=100,
         n=1,
         stop=None,
-        temperature=0.7
+        temperature=0.7,
     )
 
     print(response)
     answer = response.choices[0].message.content
     return answer
 
+
 def chatbot(request):
-    if request.method == 'POST':
-        message = request.POST.get('message')
+    if request.method == "POST":
+        message = request.POST.get("message")
         # response = "Hello, you said: " + message
         messages = [
             {
-            "role": "developer",
-            "content": [
-                {
-                "type": "text",
-                "text": "You are a helpful assistant that answers programming \
+                "role": "developer",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "You are a helpful assistant that answers programming \
                     questions in the style of a southern belle from the \
-                    southeast United States."
-                }
-            ]
+                    southeast United States.",
+                    }
+                ],
             },
-            {
-            "role": "user",
-            "content": [
-                {
-                "type": "text",
-                "text": message
-                }
-            ]
-            }
+            {"role": "user", "content": [{"type": "text", "text": message}]},
         ]
         response = ask_openai(messages)
-        return JsonResponse({'message':message, 'response': response})
-    return render(request, 'chatbot.html')
+        return JsonResponse({"message": message, "response": response})
+    return render(request, "chatbot.html")
--- django_backend/django_backend/urls.py
+++ django_backend/django_backend/urls.py
@@ -18,7 +18,4 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', include('chatbot.urls'))
-]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("chatbot.urls"))]
