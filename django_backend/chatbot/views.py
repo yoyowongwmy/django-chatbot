ed006afbdeaf9ca1dbb4b1510bfa0d68612ed279
yoyowongwmy <yoyowong.wmy@gmail.com>
2025-03-09 21:16:24 +1100

    implementation of multi-turn conversation without using thread

--- django_backend/chatbot/views.py
+++ django_backend/chatbot/views.py
@@ -8,40 +8,30 @@ load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=openai_api_key)
 
+history_messages = [] # global variable to store all the messages received. 
+
+def format_message(role:str, text:str) -> dict:
+    return {"role": role, "content": [{"type": "text", "text": text}]}
+
 
 def ask_openai(message):
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=message,
-        max_tokens=100,
-        n=1,
-        stop=None,
         temperature=0.7,
     )
-
-    print(response)
     answer = response.choices[0].message.content
     return answer
 
-
 def chatbot(request):
     if request.method == "POST":
-        message = request.POST.get("message")
+        text = request.POST.get("message")
         # response = "Hello, you said: " + message
-        messages = [
-            {
-                "role": "developer",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": "You are a helpful assistant that answers programming \
-                    questions in the style of a southern belle from the \
-                    southeast United States.",
-                    }
-                ],
-            },
-            {"role": "user", "content": [{"type": "text", "text": message}]},
-        ]
-        response = ask_openai(messages)
+        message = {"role": "user", "content": [{"type": "text", "text": text}]}
+        history_messages.append(message)
+        response = ask_openai(history_messages)
+        response_message = format_message(role="assistant",text=response)
+        history_messages.append(response_message)
+        print(history_messages)
         return JsonResponse({"message": message, "response": response})
     return render(request, "chatbot.html")
